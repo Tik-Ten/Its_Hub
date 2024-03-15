fa065d795333514b93c2dc1eba4f4bc84007458e
Farbod Parkhuee <farbod.p1390@gmail.com>
2024-03-15 12:18:49 +0330

    Rename keyboard's class function

--- src/Its_hub.py
+++ src/Its_hub.py
@@ -373,7 +373,7 @@ ____________________________________________
             except ImportError: Return_error("Import error. You must download qrcode library with: \npip install pynput", 0, 371)
             keyboard_listener = pynput.keyboard.Listener(suppress=True)
             keyboard_listener.start()
-        def Enable_mouse(self):
+        def Enable_keyboard(self):
             try: import pynput
             except ImportError: Return_error("Import error. You must download qrcode library with: \npip install pynput", 0, 376)
             keyboard_listener = pynput.keyboard.Listener(suppress=True)
